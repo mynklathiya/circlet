5c698178a97bc93eb7ec9cbceb7d1fda95b9205a
mynklathiya <132878852+mynklathiya@users.noreply.github.com>
2023-05-27 10:35:14 +0530

    Add files via upload

---
+++ c-23.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+int i,j,n=5;
+for(i=n;i>=1;i--)
+  {
+	for(j=n;j>=i;j--)
+	{
+		if(j%2==0)
+	{
+		printf("0");
+	}	
+	else
+	{
+		printf("1");
+	}
+	}
+	printf("\n");
+  }
+}
